4a18d359119bfef83cfe4eee8e00cf76db27ed58
Ivan Hlynka <ivanglinka20mail.com>
2023-01-20 05:08:55 +0200

    Furni v 1.0

---
+++ js/scripts.js
@@ -0,0 +1,61 @@
+// Custom Scripts
+// Burger Mobile Menu
+function burgerMenu() {
+  const burger = document.querySelector('.burger')
+  const menu = document.querySelector('.menu')
+  const body = document.querySelector('body')
+  burger.addEventListener('click', () => {
+    if (!menu.classList.contains('active')) {
+      menu.classList.add('active')
+      burger.classList.add('active-burger')
+      body.classList.add('locked')
+    } else {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    }
+  })
+  // Вот тут мы ставим брейкпоинт навбара
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 991.98) {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    }
+  })
+}
+burgerMenu();
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 1,
+	spaceBetween: 30,
+	loop: true,  
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	// And if we need scrollbar
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+//   // Responsive breakpoints
+//   breakpoints: {
+//   // when window width is >= 320px
+//   320: {
+//     slidesPerView: 3,
+//     spaceBetween: 20
+//   },
+//   // when window width is >= 480px
+//   480: {
+//     slidesPerView: 3,
+//     spaceBetween: 30
+//   },
+// }
+});;
