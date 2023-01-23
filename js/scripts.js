ac5e777a77bfb337a231642379f7a17c9ebef049
Ivan Hlynka <ivanglinka20mail.com>
2023-01-23 04:13:49 +0200

    ojjo v1.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,78 @@
-// Custom Scripts
-// Burger Mobile Menu
+// ********** Burger Mobile Menu **********
 function burgerMenu() {
-  const burger = document.querySelector('.burger')
-  const menu = document.querySelector('.menu')
-  const body = document.querySelector('body')
-  burger.addEventListener('click', () => {
-    if (!menu.classList.contains('active')) {
-      menu.classList.add('active')
-      burger.classList.add('active-burger')
-      body.classList.add('locked')
+  const burger = document.querySelector(".burger");
+  const menu = document.querySelector(".menu");
+  const body = document.querySelector("body");
+  burger.addEventListener("click", () => {
+    if (!menu.classList.contains("active")) {
+      menu.classList.add("active");
+      burger.classList.add("active-burger");
+      body.classList.add("locked");
     } else {
-      menu.classList.remove('active')
-      burger.classList.remove('active-burger')
-      body.classList.remove('locked')
+      menu.classList.remove("active");
+      burger.classList.remove("active-burger");
+      body.classList.remove("locked");
     }
-  })
+  });
   // Вот тут мы ставим брейкпоинт навбара
-  window.addEventListener('resize', () => {
+  window.addEventListener("resize", () => {
     if (window.innerWidth > 991.98) {
-      menu.classList.remove('active')
-      burger.classList.remove('active-burger')
-      body.classList.remove('locked')
+      menu.classList.remove("active");
+      burger.classList.remove("active-burger");
+      body.classList.remove("locked");
     }
-  })
+  });
 }
 burgerMenu();
+
+// ********** Active Menu **********
+function menuActive() {
+  function addClassElementEvent(element, className, event) {
+    let elements = document.querySelectorAll(element);
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].addEventListener(event, function (event) {
+        [].forEach.call(elements, function (el) {
+          el.classList.remove(className);
+        });
+        this.classList.add(className);
+      });
+    }
+  }
+  addClassElementEvent(".menu__item-link", "menu__item-link-active", "click");
+}
+menuActive();
+
+// ********** Active Button **********
+function btnActive() {
+  const sections = document.querySelectorAll("section[id]");
+  window.addEventListener("scroll", btnActive);
+  function addClassElementEvent(element, className, event) {
+    let elements = document.querySelectorAll(element);
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].addEventListener(event, function (event) {
+        [].forEach.call(elements, function (el) {
+          el.classList.remove(className);
+        });
+        this.classList.add(className);
+      });
+    }
+  }
+  addClassElementEvent(".hero__btn", "hero__btn-active", "click");
+}
+btnActive();
 const swiper = new Swiper('.swiper', {
 	slidesPerView: 1,
 	spaceBetween: 30,
 	loop: true,  
-	// If we need pagination
 	pagination: {
 		el: '.swiper-pagination',
 		clickable: true,
 	},
-
-	// Navigation arrows
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
-
-	// And if we need scrollbar
 	scrollbar: {
 		el: '.swiper-scrollbar',
 	},
-//   // Responsive breakpoints
-//   breakpoints: {
-//   // when window width is >= 320px
-//   320: {
-//     slidesPerView: 3,
-//     spaceBetween: 20
-//   },
-//   // when window width is >= 480px
-//   480: {
-//     slidesPerView: 3,
-//     spaceBetween: 30
-//   },
-// }
-});;
+});
